7f7fb6e63f705250df01b813a7ee2fe1d3d5aa9b
Aleksandralucz <luczynska.aleksandraa@gmail.com>
2021-07-20 08:54:51 +0200

    connected

---
+++ src/main/java/aluczynska/budgetingprogram/BudgetingProgramApplication.java
@@ -0,0 +1,13 @@
+package aluczynska.budgetingprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetingProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetingProgramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aluczynska/budgetingprogram/BudgetingProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package aluczynska.budgetingprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetingProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
